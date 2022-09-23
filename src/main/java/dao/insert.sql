c1347a6a6e900e53d6064bae752308df686e9c33
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-09-24 11:35:44 +1200

    creating sql files

---
+++ src/main/java/dao/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO PRODUCT VALUES(1, 'Dalbelo Boots', 'Green boots', 'Boots', 800.00, 12.00);
+
+INSERT INTO PRODUCT VALUES(2, 'Solomon Poles', 'Black poles', 'Poles', 300.00, 9.00);
+
+INSERT INTO PRODUCT VALUES(3, 'Burton Jacket', 'Orange jacket', 'Boots', 500.00, 5.00);
+
+INSERT INTO PRODUCT VALUES(4, 'Volcolm Pants', 'Green boots', 'Boots', 450.00, 16.00);
+
+INSERT INTO PRODUCT VALUES(5, 'Smith Goggles', 'Blue goggles', 'Goggles', 250.00, 10);
+
+INSERT INTO PRODUCT VALUES(6, 'Anon Goggles', 'Black goggles', 'Goggles', 300.00, 3);
+
+INSERT INTO PRODUCT VALUES(7, 'Volkl ski Boots', 'White boots', 'Boots', 850.00, 1);
+
+INSERT INTO PRODUCT VALUES(8, 'Spyder Goggles', 'Pink Goggles', 'Goggles', 200.00, 14);
+
+INSERT INTO PRODUCT VALUES(9, 'Mons Royale Socks', 'Green socks', 'Accessories', 30.00, 20);
+
+INSERT INTO PRODUCT VALUES(10, 'IceBreaker Beanie', 'White Striped beanie', 'Accessories', 50.00, 10);
